bf6cf846befa8005afb4228f9a02b0bdd0627d59
niszeto <niszeto@gmail.com>
2019-02-07 20:20:17 -0800

    changed style on start screen: catch me button and screen is styled

--- screens/Start.js
+++ screens/Start.js
@@ -1,35 +1,108 @@
 import React from "react";
-import { StyleSheet, View, Text, Image, Button } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableOpacity
+} from "react-native";
 
 import questionMark from "../assets/question-mark.png";
 
 const Start = props => {
   return (
-    <View style={styles.container}>
-      <Image source={questionMark} style={styles.imageStyle} />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>{/* circles */}</View>
 
-      <Text>Who's That Pokemon?</Text>
+      <View style={styles.mainContentContainer}>
+        <View style={styles.screenContainer}>
+          <Image source={questionMark} style={styles.imageStyle} />
+        </View>
+      </View>
 
-      <Button title="Catch" onPress={() => display(props)} />
-    </View>
+      <View style={styles.footerContainer}>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => props.navigation.navigate("Display")}
+        >
+          <View style={styles.buttonTextContainer}>
+            <Text style={styles.textStyle}>Catch Me!</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
-};
-
-display = props => {
-  return props.navigation.navigate("Display");
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#FF373D"
+  },
+
+  headerContainer: {
+    flex: 1,
+    width: "100%"
+  },
+
+  mainContentContainer: {
+    flex: 5,
+    width: "100%",
     justifyContent: "center",
     alignItems: "center"
   },
 
+  screenContainer: {
+    height: "95%",
+    width: "95%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    borderWidth: 10,
+    borderColor: "black",
+    backgroundColor: "#FFFFFF"
+  },
+
   imageStyle: {
-    height: 200,
-    width: 200,
+    height: 350,
+    width: 250,
     resizeMode: "contain"
+  },
+
+  footerContainer: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  buttonContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5F8F3",
+    height: "80%",
+    width: "90%",
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: "black"
+  },
+
+  buttonTextContainer: {
+    backgroundColor: "#01FFFF",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "85%",
+    width: "95%",
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: "black"
+  },
+
+  textStyle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textDecorationLine: "underline"
   }
 });
 
